76efb36547bba8642f3bb7487d3f124fc563d90a
Bruno <brunosartori.webmaster@gmail.com>
2019-08-16 07:27:11 -0300

    teste

--- seeds/20180131091933-fornecedor.js
+++ seeds/20180131091933-fornecedor.js
@@ -2,32 +2,27 @@ import datasource from '../datasource';
 
 const { newDb, oldDb } = datasource();
 
-const binaryConvert = async (table, field) => {
-	await oldDb.query(`update ${table} set ${field} = convert(binary convert(${field} using latin1) using utf8)`);
-}
+export async function up(queryInterface) {	
+	try {
+		const fornecedores = await oldDb.query('select * from cadastro_fornecedor', { type: oldDb.QueryTypes.SELECT });
 
-export async function up(queryInterface) {
-	
-	const fornecedores = await oldDb.query('select * from cadastro_fornecedor', { type: oldDb.QueryTypes.SELECT });
+		const data = [];
 
-	const data = [];
+		for (let i = 0; i < fornecedores.length; i++) {
+			const obj = fornecedores[i];
 
-	for (let i = 0; i < fornecedores.length; i++) {
-		const obj = fornecedores[i];
+			const fornecedor = {
+				id: obj.for_id,
+				nomeFantasia: obj.for_nome_fantasia,
+				tipoPessoa: obj.for_tipo_pessoa,
+				obs: obj.for_obs,
+				createdAt: obj.for_data_cadastro,
+				pessoasFk: parseInt(obj.for_cod_pessoa.toString(), 10)
+			}
 
-		const fornecedor = {
-			id: obj.for_id,
-			nomeFantasia: obj.for_nome_fantasia,
-			tipoPessoa: obj.for_tipo_pessoa,
-			obs: obj.for_obs,
-			createdAt: obj.for_data_cadastro,
-			pessoasFk: parseInt(obj.for_cod_pessoa.toString(), 10)
+			data.push(fornecedor);
 		}
 
-		data.push(fornecedor);
-	}
-
-	try {
 		await queryInterface.bulkInsert('fornecedores', data);
 	} catch (error) {
 		console.error(error);
